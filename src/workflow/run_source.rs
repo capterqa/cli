ef9abdd8b0b56400173367b6816bd7db8e146288
Filip Stefansson <filip.stefansson@gmail.com>
2021-02-23 09:32:56 +0100

    feat: read github actions event file (#24)

--- src/workflow/run_source.rs
+++ src/workflow/run_source.rs
@@ -1,8 +1,8 @@
 use crate::CliOptions;
 use last_git_commit::LastGitCommit;
-use serde::Serialize;
+use serde::{Deserialize, Serialize};
 use serde_json::json;
-use std::env;
+use std::{env, fs};
 
 #[derive(Debug, Serialize)]
 #[allow(non_camel_case_types)]
@@ -15,6 +15,29 @@ impl Default for Source {
     fn default() -> Self {
         Source::cli
     }
+}
+
+/// Partial of the event payload on GitHub Actions
+///
+/// There's a lot of extra information in `workflow/event.json` that
+/// we can use to display useful information to the user. We read it
+/// through the `GITHUB_EVENT_PATH` environment variable.
+#[derive(Deserialize, Serialize, Debug)]
+struct GitHubActionsEventPayload {
+    action: Option<String>,
+    after: Option<String>,
+    before: Option<String>,
+    number: Option<i32>,
+    pull_request: Option<GitHubActionsPullReqeustPayload>,
+}
+
+#[derive(Deserialize, Serialize, Debug)]
+struct GitHubActionsPullReqeustPayload {
+    after: Option<String>,
+    before: Option<String>,
+    id: Option<i32>,
+    number: Option<i32>,
+    title: Option<String>,
 }
 
 /// Information about where the run was excecuted. It will
@@ -65,15 +88,44 @@ impl RunSource {
         if source.ci == Some("GitHub Actions".to_string()) {
             source.sha = Some(env::var("GITHUB_SHA").unwrap());
             source.repository = Some(env::var("GITHUB_REPOSITORY").unwrap());
-            source.meta = Some(json!({
-                "GITHUB_HEAD_REF": env::var("GITHUB_HEAD_REF").unwrap(),
-                "GITHUB_BASE_REF": env::var("GITHUB_BASE_REF").unwrap(),
-                "GITHUB_WORKFLOW": env::var("GITHUB_WORKFLOW").unwrap(),
-                "GITHUB_RUN_ID": env::var("GITHUB_RUN_ID").unwrap(),
-                "GITHUB_ACTOR": env::var("GITHUB_ACTOR").unwrap(),
-            }))
+            let mut meta = json!({
+                "env": {
+                    "GITHUB_HEAD_REF": env::var("GITHUB_HEAD_REF").unwrap(),
+                    "GITHUB_BASE_REF": env::var("GITHUB_BASE_REF").unwrap(),
+                    "GITHUB_WORKFLOW": env::var("GITHUB_WORKFLOW").unwrap(),
+                    "GITHUB_RUN_ID": env::var("GITHUB_RUN_ID").unwrap(),
+                    "GITHUB_ACTOR": env::var("GITHUB_ACTOR").unwrap(),
+                }
+            });
+
+            // try to read event file
+            if let Some(event) = read_github_workflow_event() {
+                meta["event"] = json!(event);
+            }
+
+            source.meta = Some(meta);
         }
 
         source
     }
+}
+
+/// Reads the event.json file in GitHub Actions.
+///
+/// This file mimics the payload you would get in a webhook from GitHub
+/// and what we are looking for here is mainly information about PRs,
+/// like number and title.
+fn read_github_workflow_event() -> Option<GitHubActionsEventPayload> {
+    let path = env::var("GITHUB_EVENT_PATH").unwrap();
+
+    return match fs::read_to_string(&path) {
+        Ok(s) => {
+            if let Ok(payload) = serde_json::from_str(&s) {
+                return Some(payload);
+            }
+
+            None
+        }
+        _ => None,
+    };
 }
