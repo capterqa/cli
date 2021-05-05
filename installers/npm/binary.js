b54d37658568ad7e43aa171919687812c7f61238
Filip Stefansson <filip.stefansson@gmail.com>
2021-05-05 13:34:05 +0200

    fix: remove supported arm64 target

--- installers/npm/binary.js
+++ installers/npm/binary.js
@@ -30,12 +30,12 @@ const supportedPlatforms = [
     RUST_TARGET: 'x86_64-apple-darwin',
     BINARY_NAME: name,
   },
-  {
-    TYPE: 'Darwin',
-    ARCHITECTURE: 'arm64',
-    RUST_TARGET: 'arm64-apple-darwin',
-    BINARY_NAME: name,
-  },
+  // {
+  //   TYPE: 'Darwin',
+  //   ARCHITECTURE: 'arm64',
+  //   RUST_TARGET: 'arm64-apple-darwin',
+  //   BINARY_NAME: name,
+  // },
 ];
 
 const getPlatformMetadata = () => {
